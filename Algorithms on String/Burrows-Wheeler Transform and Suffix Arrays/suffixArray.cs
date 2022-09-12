c15a77f27652f6e7c14bd4400f3c682750d06b60
sharyash81 <sharyash81@gmail.com>
2022-09-12 05:45:55 +0430

    Construct the suffix array of a string

---
+++ Algorithms on String/Burrows-Wheeler Transform and Suffix Arrays/suffixArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmOnString_W2Q4
+{
+    public class W2Q4
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            int n = text.Length;
+            List<string> Suffixes = new List<string>(n);
+            for (int i = 0; i < n; i++) Suffixes.Add(text.Substring(n - i - 1));
+            Suffixes.Sort();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(n - Suffixes[i].Length + " ");
+            }
+            
+        }
+    }
+
+
+}
